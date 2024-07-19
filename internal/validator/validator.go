79d3d608dfadd8cb6ac424fae5ad1caff252dab0
Pranaenae <gnurugyanarp17@gmail.com>
2024-07-19 09:26:32 +0545

    Hash password and email validation features

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -1,6 +1,7 @@
 package validator
 
 import (
+	"regexp"
 	"strings"
 	"unicode/utf8"
 )
@@ -8,6 +9,8 @@ import (
 type Validator struct {
 	FieldErrors map[string]string
 }
+
+var EmailRX = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
 func (v *Validator) Valid() bool {
 	return len(v.FieldErrors) == 0
@@ -31,6 +34,14 @@ func (v *Validator) CheckField(ok bool, key, message string) {
 
 func NotBlank(value string) bool {
 	return strings.TrimSpace(value) != ""
+}
+
+func MinChars(value string, n int) bool {
+	return utf8.RuneCountInString(value) >= n
+}
+
+func Matches(value string, rx *regexp.Regexp) bool {
+	return rx.MatchString(value)
 }
 
 func MaxChars(value string, n int) bool {
