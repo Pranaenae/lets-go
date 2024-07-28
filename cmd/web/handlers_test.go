3f82a99b492cad5c6cf93f5385b98f23b331a5e7
Pranaenae <gnurugyanarp17@gmail.com>
2024-07-28 10:15:33 +0545

    Unit tests for routes

--- cmd/web/handlers_test.go
+++ cmd/web/handlers_test.go
@@ -1,34 +1,22 @@
 package main
 
 import (
-	"bytes"
-	"io"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"github.com/Pranaenae/lets-go/internal/assert"
 )
 
 func TestPing(t *testing.T) {
-	rr := httptest.NewRecorder()
 
-	r, err := http.NewRequest(http.MethodGet, "/", nil)
-	if err != nil {
-		t.Fatal(err)
-	}
+	app := NewTestApplication(t)
+	ts := newTestServer(t, app.routes())
 
-	ping(rr, r)
+	defer ts.Close()
 
-	rs := rr.Result()
+	code, _, body := ts.get(t, "/ping")
 
-	assert.Equal(t, rs.StatusCode, http.StatusOK)
+	assert.Equal(t, code, http.StatusOK)
 
-	defer rs.Body.Close()
-	body, err := io.ReadAll(rs.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	bytes.TrimSpace(body)
-	assert.Equal(t, string(body), "OK")
+	assert.Equal(t, body, "OK")
 }
